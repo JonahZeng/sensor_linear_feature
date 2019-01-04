9c49e4402a595b09f9e73b3a380da3fd768e1276
Jonah Zeng <302826183@qq.com>
2019-01-04 14:43:19 +0800

    Add files via upload

---
+++ inc/blcxmlhighlight.h
@@ -0,0 +1,15 @@
+﻿#ifndef BLCXMLHIGHLIGHT_H
+#define BLCXMLHIGHLIGHT_H
+
+#include <QSyntaxHighlighter>
+#include <QTextDocument>
+
+class BlcXmlHighlight : public QSyntaxHighlighter
+{
+public:
+    explicit BlcXmlHighlight(QTextDocument* parent);
+protected:
+    void highlightBlock(const QString &text);
+};
+
+#endif // BLCXMLHIGHLIGHT_H
