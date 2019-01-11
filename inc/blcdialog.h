2ccab3644b62ca77924c74e84126eb8de66bc18c
Jonah Zeng <302826183@qq.com>
2019-01-11 20:12:15 +0800

    Update blcdialog.h

--- inc/blcdialog.h
+++ inc/blcdialog.h
@@ -15,6 +15,24 @@
 #include <QPlainTextEdit>
 #include <qdom.h>
 #include <QSurface3DSeries>
+#include <QComboBox>
+#include <QSpinBox>
+#include <QLineEdit>
+
+class BlcSpinBox: public QSpinBox
+{
+    Q_OBJECT
+public:
+    BlcSpinBox(QWidget* parent, bool isMin);
+    ~BlcSpinBox(){}
+    void stepBy(int steps);
+public:
+    void setBro(BlcSpinBox* b){bro=b;}
+private:
+    BlcSpinBox* bro;
+    bool isMin;
+};
+//---------------------------------------------------------
 
 class gridImgLabel : public QLabel
 {
@@ -51,11 +69,22 @@ protected slots:
     void onSaveXmlButton();
     void onSaveAsButton();
     void onContextChanged();
-
+    void onSurface_r_Toggled(bool statu);
+    void onSurface_gr_Toggled(bool statu);
+    void onSurface_gb_Toggled(bool statu);
+    void onSurface_b_Toggled(bool statu);
+    void onSelectionNoItem(bool statu);
+    void onSelectionSingelItem(bool statu);
+    void onSelectionRow(bool statu);
+    void onSelectionColumn(bool statu);
+    void onThemeListIdxChanged(int idx);
+    void onYminValueChanged(int val);
+    void onYmaxValueChanged(int val);
 
 private:
     QLabel* useTip;
     QTreeWidget* treeWgt;
+    quint16 iso;
     gridImgLabel* imgView;
     QGroupBox* grid_box;
     QRadioButton *noGrid, *grid5_5, *grid11_11;
@@ -81,13 +110,22 @@ private:
     Q3DSurface* threeDSurface;
     QWidget* surfaceContainerWgt;
     QRadioButton *surface_r, *surface_gr, *surface_gb, *surface_b;
+    QRadioButton *selection_no, *selection_item, *selection_row, *selection_col;
+    BlcSpinBox *yMinSpinBox, *yMaxSpinBox;
     QMap<quint16, SurfaceDateArrP_4> aeGain_surfaceData_4p_map;
 
     QSurface3DSeries showOnSreenSeries;
+    QSurfaceDataArray* showOnScreenDataArr;
+
+    QComboBox *themeList;
 
 
 private:
     void showRawFile(const QString& rawfile);
+    bool deepCopyDataArray(QSurfaceDataArray* dst, const QMap<quint16, SurfaceDateArrP_4>& src_map, quint16 iso, int selectBayer);//根据iso, r/gr/gb/b选项选择复制data array
+    void setLeftUI();
+    void setMiddleUI();
+    void setRightUI();
 
 };
 
