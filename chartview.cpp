94080b45982f1b618c5f6c2299a74665bfcc649b
Jonah Zeng <302826183@qq.com>
2019-01-18 17:57:52 +0800

    Update chartview.cpp

--- chartview.cpp
+++ chartview.cpp
@@ -1,5 +1,6 @@
-﻿#include "inc/chartview.h"
+#include "inc/chartview.h"
 #include <QtCharts/QLineSeries>
+#include <QtCharts/QValueAxis>
 
 ChartView::ChartView(Chart *chart, QWidget *parent):
     QChartView(chart, parent),
@@ -42,15 +43,48 @@ void ChartView::keyPressEvent(QKeyEvent *event)
 void ChartView::mouseMoveEvent(QMouseEvent *event)
 {
     qint16 series_press_flag = ((Chart*)this->chart())->getSeriseNum();
-    if(series_press_flag == Chart::UN_PRESSED)
-        return;
 
+    if(series_press_flag < Chart::NLC_SPECIFIC_LINE_PRESSED && series_press_flag >= 0){
+        QPoint global_p = event->pos();
+        QPointF scene_p = mapToScene(global_p);
+        QPointF chart_p = this->chart()->mapFromScene(scene_p);
+        QPointF series_p =  this->chart()->mapToValue(chart_p, this->chart()->series().at(series_press_flag));
+        //qDebug()<<series_p<<endl;
+        quint32 idx = static_cast<Chart*>(this->chart())->getSeletedIdx();
+        qreal origin_x = ((QLineSeries*)(this->chart()->series().at(series_press_flag)))->at(idx).x();
+        ((QLineSeries*)(this->chart()->series().at(series_press_flag)))->replace(idx, origin_x, series_p.y()); //x不动，只动y值
+    }
+    else if(series_press_flag == Chart::NLC_SPECIFIC_LINE_PRESSED){
+        QPoint global_p = event->pos();
+        QPointF scene_p = mapToScene(global_p);
+        QPointF chart_p = this->chart()->mapFromScene(scene_p);
+        QPointF series_p =  this->chart()->mapToValue(chart_p, this->chart()->series().at(series_press_flag));
+        qreal origin_0_x = ((QLineSeries*)(this->chart()->series().at(series_press_flag)))->at(0).x();
+        qreal origin_1_x = ((QLineSeries*)(this->chart()->series().at(series_press_flag)))->at(1).x();
+        QVector<QPointF> new_line = {QPointF(origin_0_x, series_p.ry()), QPointF(origin_1_x, series_p.ry())};
+        ((QLineSeries*)(this->chart()->series().at(series_press_flag)))->replace(new_line);
+    }
+    else if(series_press_flag == Chart::UN_PRESSED)
+        return;
+}
+
+void ChartView::mousePressEvent(QMouseEvent *event)//假如点击的点在
+{
     QPoint global_p = event->pos();
     QPointF scene_p = mapToScene(global_p);
     QPointF chart_p = this->chart()->mapFromScene(scene_p);
-    QPointF series_p =  this->chart()->mapToValue(chart_p, this->chart()->series().at(series_press_flag));
-    //qDebug()<<series_p<<endl;
-    quint32 idx = static_cast<Chart*>(this->chart())->getSeletedIdx();
-    qreal origin_x = ((QLineSeries*)(this->chart()->series().at(series_press_flag)))->at(idx).x();
-    ((QLineSeries*)(this->chart()->series().at(series_press_flag)))->replace(idx, origin_x, series_p.y()); //x不动，只动y值
+    QPointF series_p = this->chart()->mapToValue(chart_p, this->chart()->series().at(Chart::NLC_SPECIFIC_LINE_PRESSED));
+    QPointF first_p = ((QLineSeries*)(this->chart()->series().at(Chart::NLC_SPECIFIC_LINE_PRESSED)))->at(0);
+    qreal nlc_specific_line_y = first_p.ry();
+    qreal axisY_unit = (((QValueAxis*)this->chart()->axisY())->max() - ((QValueAxis*)this->chart()->axisY())->min())/100;
+    if(series_p.ry()> nlc_specific_line_y-axisY_unit && series_p.ry()<nlc_specific_line_y+axisY_unit){
+        ((Chart*)this->chart())->setPressedSeriseNum(Chart::NLC_SPECIFIC_LINE_PRESSED);
+    }
+    QChartView::mousePressEvent(event);
+}
+
+void ChartView::mouseReleaseEvent(QMouseEvent *event)
+{
+    ((Chart*)this->chart())->setPressedSeriseNum(Chart::UN_PRESSED);
+    QChartView::mouseReleaseEvent(event);
 }
